e0bbf89eddbdb10bc6ede64541d9bff16bb5925d
friken <friken@gmail.com>
2014-04-27 15:52:15 -0500

    cleanup of textures - kid isn't armless anymore

---
+++ Assets/Scripts/Intro.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Intro : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    IEnumerator coStartIntro()
+    {
+        yield return new WaitForSeconds(1f);
+    }
+}

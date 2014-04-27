2429e5d33909a4ddce6d0d083836776be80a1421
friken <friken@gmail.com>
2014-04-27 17:25:43 -0500

    Scene fix for clipping

--- Assets/Scripts/Intro.cs
+++ Assets/Scripts/Intro.cs
@@ -2,11 +2,14 @@
 using System.Collections;
 
 public class Intro : MonoBehaviour {
+    public GameObject logo;
+    public GameObject credits;
+    public GameObject dock;
 
 	// Use this for initialization
 	void Start () 
     {
-	
+        StartCoroutine(coStartIntro());
 	}
 	
 	// Update is called once per frame
@@ -17,6 +20,22 @@ public class Intro : MonoBehaviour {
 
     IEnumerator coStartIntro()
     {
-        yield return new WaitForSeconds(1f);
+        //fadein logo
+        Color origCLR = logo.renderer.material.GetColor("_Color");
+        Color clr = origCLR;
+        float dir = 1;
+        for (int i = 0; i < 200; i++ )
+        {
+            if (i == 100)
+            {
+                dir = -1;
+                yield return new WaitForSeconds(1.0f);
+            }
+                
+            clr.a += 0.01f*dir;
+            logo.renderer.materials[0].SetColor("_Color", clr);
+            yield return new WaitForSeconds(0.04f);
+        }
+            yield return new WaitForSeconds(1f);
     }
 }

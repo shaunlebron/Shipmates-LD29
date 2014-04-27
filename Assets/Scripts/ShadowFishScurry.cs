0f167172a97da9e64fd012b22f2a1e12977e1d4f
jp <dreammjpr@gmail.com>
2014-04-27 15:46:10 -0500

    Fish running away
    First working version of ShadowFishScurry

---
+++ Assets/Scripts/ShadowFishScurry.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShadowFishScurry : MonoBehaviour 
+{
+	Vector3 initialPosition;
+	float swimVelocity = 5.0f;
+	float leftLimit = -16.5f;
+	float rightLimit = 16.5f;
+
+	// Use this for initialization
+	void Start () 
+    { 
+		initialPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		Vector3 scale = transform.localScale;
+		Vector3 pos = transform.position;
+		if(transform.position.x > rightLimit || transform.position.x < leftLimit )
+		{
+			Destroy(this);
+		}
+		else
+		{
+			if(scale.y > 0 )
+				transform.position = new Vector3(pos.x - swimVelocity * Time.deltaTime, pos.y, pos.z);
+			if(scale.y < 0 )
+				transform.position = new Vector3(pos.x + swimVelocity * Time.deltaTime, pos.y, pos.z);
+		}
+			
+		Debug.Log(pos.x);
+	}
+
+    void FixedUpdate()
+    {
+        
+    }
+
+
+}

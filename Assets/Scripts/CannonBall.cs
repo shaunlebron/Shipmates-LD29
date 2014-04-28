c4abf11c40ce016084fa6437a49859b68e76ac72
Jason Bunn <jason.bunn009@gmail.com>
2014-04-27 20:32:21 -0500

    Cannon code cleanup
    Removed extraneous code, cleaned up distance formula.

--- Assets/Scripts/CannonBall.cs
+++ Assets/Scripts/CannonBall.cs
@@ -8,35 +8,34 @@ public class CannonBall : MonoBehaviour {
 
 	private MeshRenderer ballRenderer;
 	private ParticleSystem smokeParticle;
-	private float fTimeToLive = 2f;
-	private float fMaxHeight = -4.5f;
+	
+	private float fMaxHeight;
 
 	public float LifeModifier { get; set; }
 
 	private Rigidbody m_MyRB;
 
-	private float m_fLifeTimer;
+	
 	private Vector3 m_OriginPos;
 
 	private Vector3 m_fGravity;
 	private Transform m_MyTransform;
-	private float m_fHalfTime;
-	private float m_fStartTime;
-	private bool m_bProjectileReachedApex = false;
+	
+	
 	private bool m_bFishSpawned = false;
 
 	// Use this for initialization
 	void Start () {
 
-		m_fHalfTime = 9999;
-		m_fStartTime = Time.time;
+		
 		m_MyRB = rigidbody;
 		m_fGravity = Physics.gravity;
-		m_fLifeTimer = Time.time + LifeModifier;
+		
 		m_MyTransform = transform;
 		smokeParticle = m_MyTransform.FindChild ("BallParticles").GetComponent<ParticleSystem>();
 		ballRenderer = GetComponent<MeshRenderer> ();
 		m_OriginPos = m_MyTransform.position;
+		fMaxHeight = m_OriginPos.y;
 	}
 	
 	// Update is called once per frame
